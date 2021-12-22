fd11e916ecff05c5c5607195985d10956f65a0ae
Ali Elmorsy <ali.fci_0997@fci.kfs.edu.eg>
2021-12-22 21:21:47 +0200

    Asdw

---
+++ Security/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Security
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] b = new byte[32];
+           // Array.Fill(b, (byte)1);
+            var rc5 = new RC5(b, 32,5);
+            rc5.Encrypt("A04F5C1DA04F5C1D");
+        }
+    }
+}
